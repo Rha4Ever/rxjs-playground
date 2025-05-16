157456594cb493bad46dbcebf2ae4aa566763a34
RolandHaslauer <roland.haslauer@seetickets.ch>
2025-05-16 11:32:51 +0200

    first commit

---
+++ src/app/exercise-creating/exercise-creating.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { Observable, of, from, timer, interval, ReplaySubject, map, filter } from 'rxjs';
+
+import { HistoryWindow } from '../shared/history-window/history-window';
+
+@Component({
+  templateUrl: './exercise-creating.ng.html',
+  imports: [HistoryWindow]
+})
+export class ExerciseCreating {
+
+  logStream$ = new ReplaySubject<unknown>();
+
+  constructor() {
+    /**
+     * 1. Erstelle ein Observable und abonniere den Datenstrom.
+     *    Probiere dazu die verschiedenen Creation Functions aus: of(), from(), timer(), interval()
+     * 2. Implementiere außerdem ein Observable manuell, indem du den Konstruktor "new Observable()" nutzt.
+     *
+     * Tipps:
+     * Zum Abonnieren kannst du einen (partiellen) Observer oder ein einzelnes next-Callback verwenden.
+     * Du kannst die Methode this.log() verwenden, um eine Ausgabe in der schwarzen Box im Browser zu erzeugen.
+     */
+
+    /******************************/
+    const observable = of('😊', '😆', '😎');
+
+    const observer = {
+      next: (e: string) => this.log(e),
+      error: (err: any) => this.log('Error' + err),
+      complete: () => this.log('Complete')
+    }
+
+    observable.subscribe(observer)
+
+    
+    /******************************/
+  }
+
+  log(msg: unknown) {
+    this.logStream$.next(msg);
+  }
+
+}
---
+++ src/app/exercise-gamescore/exercise-gamescore.ts
@@ -0,0 +1,50 @@
+import { Component, signal } from '@angular/core';
+import { Subject, ReplaySubject, scan, reduce } from 'rxjs';
+
+import { HistoryWindow } from '../shared/history-window/history-window';
+
+@Component({
+  templateUrl: './exercise-gamescore.ng.html',
+  imports: [HistoryWindow]
+})
+export class ExerciseGamescore {
+
+  logStream$ = new ReplaySubject<unknown>();
+  readonly score$ = new Subject<number>();
+
+  readonly currentScore = signal(0);
+  readonly finalScore = signal<number | undefined>(undefined);
+
+  constructor() {
+    /**
+     * Wir entwickeln ein spannendes Browser-Spiel!
+     * Jetzt fehlt nur noch der Code, um den Punktestand zu ermitteln ...
+     */
+
+    /******************************/
+
+    this.score$.pipe(
+      scan((acc, item) => acc + item, 0)
+    ).subscribe(score => this.currentScore.set(score));
+
+    this.score$.pipe(
+      reduce((acc, item) => acc + item, 0)
+    ).subscribe(score => this.finalScore.set(score));
+    
+    /******************************/
+
+    this.score$.subscribe({
+      next: e => this.logStream$.next(e),
+      complete: () => this.logStream$.next('✅ COMPLETE')
+    });
+  }
+
+  finishGame() {
+    this.score$.complete();
+  }
+
+  addScore(amount: number) {
+    this.score$.next(amount);
+  }
+
+}
